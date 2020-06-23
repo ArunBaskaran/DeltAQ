16079362f39552e5b8e2888989c60f559789e078
ArunBaskaran <arupad@gmail.com>
2020-06-23 09:26:56 -0400

    Added comments and explanation

--- aws-implementation/Livedata_Ingestion/livedata_ndjson.py
+++ aws-implementation/Livedata_Ingestion/livedata_ndjson.py
@@ -6,24 +6,24 @@ from pyspark.sql.types import *
 import pyspark.sql.functions as f
 from pyspark.sql.functions import *
     
-def download_from_s3(response):
+#------------------------UDFs--------------------------#
+
+def download_from_s3(response):     #Download file from s3 onto local ec2 instance
     data = json.loads(response['Messages'][0]['Body'])
     data1 = json.loads(data['Message'])
     file = data1['Records'][0]['s3']['object']['key']
     print("Downloading ", file)
     name = '/home/ubuntu/file' + str(flag) + '.ndjson'
     s3.download_file('openaq-fetches', file, name)
-    receipt_handle = response['Messages'][0]['ReceiptHandle']
-    sqs.delete_message(QueueUrl=queue_url, ReceiptHandle=receipt_handle)
     return name
     
-def read_from_json(filename):
+def read_from_json(filename):      #Load json files into pyspark dataframes
     url = XXX
     properties = YYY 
     df = spark.read.json(filename)
     return df 
     
-def schema_transformation(df):
+def schema_transformation(df):      #Transform raw data from a nested structure to a linear structure
     df = df.drop('attribution')
     df = df.withColumn("time", col('date.local').cast(TimestampType()))
     df = df.drop('date')
@@ -37,7 +37,7 @@ def schema_transformation(df):
     df = df.withColumn("day", (dayofyear(col('time').cast(DateType())).cast(IntegerType())))
     return df
     
-def write_to_tables(df):
+def write_to_tables(df):            #Write cross-joined dataframe tio new table
     df = df.filter(df['country'] == "US")
     urlzip = XXXX
     properties = YYYY
@@ -51,6 +51,7 @@ def write_to_tables(df):
     df3 = df.select(df['time'], df['week'], df['day'], df['zip'], df['parameter'], df['unit'], df['value'])
     df3.write.jdbc(url = url, table = 'db_name3', mode = 'append', properties=properties)
 
+#----------------------------main-----------------------------#
 
 if __name__ == "__main__":
     s3 = boto3.client('s3', region_name='us-east-1')
@@ -61,7 +62,7 @@ if __name__ == "__main__":
 
     spark = SparkSession\
             .builder\
-            .appName("Downloading from local files")\
+            .appName("Ingestion of live data")\
             .config('spark.driver.extraClassPath', '/usr/local/spark/jars/postgresql-42.2.13.jar')\
             .getOrCreate()
 
@@ -69,12 +70,14 @@ if __name__ == "__main__":
     while(1):
         flag += 1
         response = sqs.receive_message(QueueUrl = queue_url)
-        if(len(response)<2):
+        if(len(response)<2):  #len(response) = 2 when an unread message is on the queue
             break
         name = download_from_s3(response)
         df = read_from_json(name)
         df = schema_transformation(df)
         write_to_tables(df)
+        receipt_handle = response['Messages'][0]['ReceiptHandle']
+        sqs.delete_message(QueueUrl=queue_url, ReceiptHandle=receipt_handle) #Delete message after file data has been loaded onto table
         if os.path.exists(name):
             os.remove(name)
 
