359861bbb2031189ffb2d8b5c2adefa1ed2fa454
Arun Baskaran <arupad@gmail.com>
2020-06-24 16:17:09 -0400

    Modularized code for geospatial integration

---
+++ src/Geospatial_integration_Spark/configs.py
@@ -0,0 +1,4 @@
+PYSPARK_CONFIGS = '--driver-class-path /tmp/jars/postgresql-42.2.13.jar --jars /tmp/jars/postgresql-42.2.13.jar pyspark-shell'
+SESSION_NAME = "Integrating two different datasets"
+DRIVER_PATH = '/usr/local/spark/jars/postgresql-42.2.13.jar'
+
--- src/Geospatial_integration_Spark/main.py
+++ src/Geospatial_integration_Spark/main.py
@@ -3,51 +3,19 @@ from pyspark.sql import *
 from pyspark.sql.types import *
 import pyspark.sql.functions as f
 from pyspark.sql.functions import *
+from aux_funcs import *
+from configs import *
 
-
-#------------------------UDFs--------------------------#
-
-def read_from_tables():    #Load table into pyspark dataframes
-    url = XXX
-    properties = YYY 
-    df_aqraw = spark.read.jdbc(url=url,table='usa',properties=properties)
-    df_zip = spark.read.jdbc(url=url,table='zip_code',properties=properties)
-    return df_aqraw, df_zip
-    
-def filter_df_aqraw(df):     #Clean dataframes - Drop irrelevant columns, filter column values
-    df = df.select(df['country'], df['time'], df['latitude'], df['longitude'])
-    df = df.filter(f.col('country') == "US")
-    df = df.select(df['time'], df['latitude'], df['longitude'])
-    df = df.filter(f.col('time') > ('2020-06-06 00:00:00'))
-    df = df.dropDuplicates(['latitude', 'longitude'])
-    return df
-    
-def crossjoin(df1, df2):       #Pyspark computation that integrates the data from the two tables
-    df = df1.crossJoin(df2)
-    df = df.withColumn("dis_diff", (abs(df.latitude - df.lat) + abs(df.longitude - df.lng)))
-    partition_list = ["latitude", "longitude"]
-    df =  df.withColumn( "rank", dense_rank().over(Window.partitionBy([col(c) for c in partition_list]).orderBy(asc("dis_diff"))))  #Rank the distances between each lat-long pair in aq data and lat-long pair in zipcode data
-    df = df.filter(f.col('rank') == 1)  #Select records that have rank 1, i.e., the best match between the two data
-    df = df.select(df['latitude'], df['longitude'], df['zip'])
-    return df
-
-def write_to_tables(df):        #Write cross-joined dataframe to new table
-    url = XXX
-    properties = YYY 
-    df.write.jdbc(url = url, table = 'db_name_3', mode = 'append', properties=properties)
-    
-#----------------------------main-----------------------------#
 if __name__ == "__main__":
-    os.environ['PYSPARK_SUBMIT_ARGS'] = '--driver-class-path /tmp/jars/postgresql-42.2.13.jar --jars /tmp/jars/postgresql-42.2.13.jar pyspark-shell'
+    
+    os.environ['PYSPARK_SUBMIT_ARGS'] = PYSPARK_CONFIGS
 
-    spark = SparkSession\
-            .builder\
-            .appName("Integrating two different datasets")\
-            .config('spark.driver.extraClassPath', '/usr/local/spark/jars/postgresql-42.2.13.jar')\
-            .getOrCreate()
-        
+    spark = SparkSession.builder.appName(SESSION_NAME).config('spark.driver.extraClassPath', DRIVER_PATH).getOrCreate()
 
     df_aqraw, df_zip = read_from_tables()
+    
     df_aqraw = filter_df_aqraw(df_aqraw)
+    
     df_integrated = crossjoin(df_aqraw, df_zip)
+    
     spark.stop()
