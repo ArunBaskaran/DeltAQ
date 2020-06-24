67d139da2da56754cada583ca3f9a112c20bbefe
Arun Baskaran <arupad@gmail.com>
2020-06-24 15:47:06 -0400

    Created sub-directory to add PostgreSQL queries for Grafana

---
+++ src/Grafana_queries/Queries.sql
@@ -0,0 +1,21 @@
+
+/*--------------------Query for live values---------------*/
+
+SELECT
+  "time" AS "time",
+  value, (select avg(value) from phoenix b where b.week = date_part('week', a.time) and date_part('year', b.time) = 2019 and b.parameter = 'pm10') as weekly_avg
+FROM phoenix a
+WHERE
+  $__timeFilter("time") and a.parameter = 'pm10'
+ORDER BY 1
+
+/*--------------------Query for annual averages---------------*/
+
+SELECT
+  "time" AS "time",
+  value, (select avg(value) from usa b where b.zip = 85006 and b.parameter = 'pm10' and b.week = date_part('week', a.time) and date_part('year', b.time) = 2019) as weekly_avg
+FROM usa a
+WHERE
+  a.zip = 85006 and a.parameter = 'pm10' and $__timeFilter("time")
+ORDER BY 1
+
